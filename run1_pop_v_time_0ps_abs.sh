145b401e7477fe98e1482981d7dee392fd426f8e
khaister <nguyenkhai101@gmail.com>
2016-11-03 23:29:27 -0700

    Fixed calling correct script

--- run1_pop_v_time_0ps_abs.sh
+++ run1_pop_v_time_0ps_abs.sh
@@ -4,6 +4,6 @@ CUTOFF_TIME="0.0"
 INPUT="../luteo_boundary_defined_macrostates_labeled_${CUTOFF_TIME}ps.txt"
 OUTPUT="../luteo_boundary_defined_macrostates_pop_v_time_${CUTOFF_TIME}ps_abs.txt"
 
-printf "Running: ./pop-v-time.pl $INPUT $OUTPUT. . . "
-./pop-v-time.pl $INPUT $OUTPUT
+printf "Running: ./pop-v-time-abs.pl $INPUT $OUTPUT. . . "
+./pop-v-time-abs.pl $INPUT $OUTPUT
 printf "Done!"
